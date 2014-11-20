9750e838d1c0b5899bd5a0a0f670f8cf7f81291b
Russell Toris <rctoris@wpi.edu>
2014-11-20 11:04:40 -0500

    minify script

---
+++ scripts/minify.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# go to the meshes folder
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MESHES="$DIR/../meshes"
+cd $MESHES
+
+# remove old minified files
+rm *.min.dae
+
+# re-minify
+for f in *.dae ; do xmllint --noblanks $f > "${f/.dae/.min.dae}" ; done
