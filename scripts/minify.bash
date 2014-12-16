33dafa60ebe4da993b3a7652a632851b39c198f0
Russell Toris <rctoris@wpi.edu>
2014-12-16 15:05:06 -0500

    travis fix

--- scripts/minify.bash
+++ scripts/minify.bash
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-
 # go to the meshes folder
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MESHES="$DIR/../meshes"
-MATERIALS="$MESHES/materials/*.jpg"
-cd $MESHES
 
 # remove old minified files
+cd $MESHES
 rm *.min.dae
 
 # re-minify
-jpegoptim $MATERIALS
 for f in *.dae ; do xmllint --noblanks $f > "${f/.dae/.min.dae}" ; done
